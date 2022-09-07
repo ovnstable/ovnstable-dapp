53b41bc00cef94cf99f6758acc83d90c86c08d74
alexander <a.sharuev@ifrbanking.ru>
2022-09-07 15:53:51 +0300

    new wallets whitelisted, gnosis wallet added

--- src/store/modules/common/web3.js
+++ src/store/modules/common/web3.js
@@ -131,6 +131,10 @@ const actions = {
                 preferred: true
             },
             {
+                walletName: "gnosis",
+                preferred: false
+            },
+            {
                 walletName: 'ledger',
                 rpcUrl: rpcUrl,
                 preferred: false

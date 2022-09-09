0386ff9e023f70e1c331262dfd10cefd381561db
alexander <a.sharuev@ifrbanking.ru>
2022-09-09 16:16:11 +0300

    referral system merged from referral to main

--- src/store/modules/common/referral.js
+++ src/store/modules/common/referral.js
@@ -1,68 +1,53 @@
 const KEY = 'REFERRAL_CODE';
 
-const state = {
+const state = {};
 
-
-};
-
-const getters = {
-
-
-};
+const getters = {};
 
 const actions = {
 
     async getReferralCode({commit, dispatch, getters, rootState}) {
 
         const match = document.cookie.match(new RegExp('(^| )' + KEY + '=([^;]+)'));
-        if (match){
+        if (match) {
             let referralCode = match[2];
             console.log(`Referral code found in cookie = [${referralCode}]`)
             return referralCode;
-        }else {
+        } else {
             console.log('Referral code not found in cookies');
             return '';
         }
-
     },
 
 
     async initReferralCode({commit, dispatch, getters, rootState}) {
 
         const exist = document.cookie.indexOf(`${KEY}=`) !== -1;
-        if (exist){
+        if (exist) {
             console.log('REFERRAL_CODE already marked');
-        }else {
+        } else {
 
             const queryString = window.location.search;
             const urlParams = new URLSearchParams(queryString);
             const referralValue = urlParams.get('referral');
 
-            if (referralValue){
+            if (referralValue) {
                 let days = 86400 * 30 // 86400 = 1 day
 
                 console.log(`Referral [${referralValue}] found in URL`);
 
-                const expire = (new Date(Date.now()+ days*1000)).toUTCString();
+                const expire = (new Date(Date.now() + days * 1000)).toUTCString();
                 document.cookie = `${KEY}=${referralValue}; expires=${expire};path=/;`;
 
                 console.log(`Setup [REFERRAL_CODE=${referralValue}] to cookies`);
-            }else {
+            } else {
                 console.log('Referral not found in URL');
             }
-
-
         }
-
-
-
     }
-
 };
 
-const mutations = {
-
-};
+const mutations = {};
 
 export default {
     namespaced: true,

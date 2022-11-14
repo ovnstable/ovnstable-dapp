b0fca5e88b83752322fa901ca0c02a425ff413ce
Roman <romangolovlyov@gmail.com>
2022-11-14 05:33:42 +0500

    DEV-673 Doughnut.vue updated and show date instead of time V1.

--- src/components/market/strategy/payouts/Doughnut.vue
+++ src/components/market/strategy/payouts/Doughnut.vue
@@ -5,9 +5,10 @@
       :size="size" unit="px" :thickness="20"
       :sections="sections"
       :start-angle="0" :auto-adjust-text-size="false">
-        <p class="total-label mt-5">Last payout was</p>
-        <p class="total-label-time mt-n3 mb-0">{{ timeDisplay === "00 : 00" ? '—' : timeDisplay }}</p>
-        <p class="total-label">hours ago</p>
+        <p class="total-label mt-5">Last payout was on</p>
+        <p class="total-label-time mt-n3 mb-0">{{ timestamp }}</p>
+        <p class="total-label mb-0">Please withdraw</p>
+        <p class="total-label mt-n2">your funds</p>
     </vc-donut>
 </template>
 
@@ -48,62 +49,27 @@ export default {
     components: {},
 
     data: () => ({
-        sections: [],
+        timestamp: ""
     }),
 
     computed: {
-        timeDisplay: function () {
-            let hours = Math.trunc(this.getHours());
-            let minutes = Math.trunc((this.getHours() - Math.trunc(this.getHours())) * 60.0);
 
-            if (hours < 10) {
-                hours = "0" + hours;
-            }
-
-            if (minutes < 10) {
-                minutes = "0" + minutes;
-            }
-
-            return hours + " : " + minutes;
-        },
     },
 
     mounted() {
-        this.updateSectionsData();
     },
 
     created() {
-        this.updateSectionsData();
+        setInterval(this.getNow);
     },
 
     methods: {
-        getHours() {
-            if (this.lastDate) {
-                let now = this.$moment.utc(new Date());
-                let lastPayoutDateTime = this.$moment.utc(this.lastDate);
-                let duration = this.$moment.duration(now.diff(lastPayoutDateTime));
-
-                return duration.asHours();
-            } else {
-                return 0;
-            }
-        },
-
-        getPercent(item, data) {
-            return Math. min((this.getHours() / 24.0) * 100, 100);
-        },
-
-        updateSectionsData() {
-            this.sections = [];
-
-            this.sections.push(
-                {
-                    color: this.color,
-                    label: "Time",
-                    value: this.getPercent(),
-                }
-            );
-        },
+        getNow: function() {
+            let today = new Date();
+            let date = today.getFullYear()+'.'+(today.getMonth()+1)+'.'+today.getDate();
+            const dateTime = date;
+            this.timestamp = dateTime;
+        }
     }
 }
 </script>
@@ -114,7 +80,7 @@ export default {
     font-family: 'Roboto', sans-serif;
     font-style: normal;
     font-weight: 300;
-    font-size: 18px;
+    font-size: 16px;
     line-height: 32px;
     text-align: center;
     color: var(--doughnut-text);
@@ -124,8 +90,8 @@ export default {
     font-family: 'Roboto', sans-serif;
     font-style: normal;
     font-weight: 400;
-    font-size: 54px;
-    line-height: 60px;
+    font-size: 40px;
+    line-height: 54px;
     text-align: center;
     font-feature-settings: 'pnum' on, 'lnum' on;
     color: var(--doughnut-text);

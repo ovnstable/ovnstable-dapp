a733591993f0d56c3bf030ea1f0418ee36615104
Kron1749 <drakgr98@gmail.com>
2023-11-21 16:20:29 +0300

    updated_performance_tab_insurance

--- src/views/insurance/tab/PerformanceTab.vue
+++ src/views/insurance/tab/PerformanceTab.vue
@@ -59,7 +59,8 @@ import {mapGetters} from "vuex";
 import ChartApy from "@/components/insurance/strategy/chart/ChartApy";
 import Doughnut from "@/components/market/strategy/payouts/Doughnut.vue";
 import Table from "@/components/market/strategy/payouts/InsuranceTable.vue";
-import moment from "moment/moment";
+import dayjs from "dayjs";
+import {differenceInDays} from "@/utils/dates.js"
 import {insuranceApiService} from "@/services/insurance-api-service";
 
 export default {
@@ -128,11 +129,9 @@ export default {
       },
       payouts: function () {
         let data = this.payoutsData;
-        return data ? data.sort(
-            function(o1,o2){
-              return moment(o2.date).isBefore(moment(o1.date)) ? -1 : moment(o2.date).isAfter(moment(o1.date)) ? 1 : 0;
-            }
-        ) : null;
+        return data
+            ? [...data].sort((o1, o2) => differenceInDays(o2.date, o1.date))
+            : null;
       }
     },
 
@@ -163,11 +162,8 @@ export default {
                 this.payoutsData = value;
                 strategyData = value;
 
-                strategyData.sort(
-                    function (o1, o2) {
-                        return moment(o1.date).isBefore(moment(o2.date)) ? -1 : moment(o1.date).isAfter(moment(o2.date)) ? 1 : 0;
-                    }
-                );
+                strategyData.sort((o1, o2) => differenceInDays(o1.date, o2.date));
+
 
                 let clientData = strategyData;
 
@@ -203,11 +199,10 @@ export default {
                         payout.comp =  (accumulator * 100 / startValue - 100);
                         payout.comp =  parseFloat(payout.comp ? payout.comp : 0.00).toFixed(3);
 
-                        widgetDataDict[moment(payout.date).format('DD.MM.YYYY')] = payout.comp;
-
+                        widgetDataDict[dayjs(payout.date).format('DD.MM.YYYY')] = payout.comp;
                         // date
                         if (i === 0) {
-                            compoundData.firstDate = moment(payout.date).format('MMM D, YYYY');
+                            compoundData.firstDate = dayjs(payout.date).format('MMM D, YYYY');
                         }
 
                         // week

0cbb54aaa71edb345be8cdf20ec3d7bbd97b43d1
matveyb <matvey.b2022@gmail.com>
2023-12-01 16:19:07 +0100

    remove font-awesome

--- src/App.vue
+++ src/App.vue
@@ -10,7 +10,7 @@ import {mapActions} from "vuex";
 export default {
     name: 'App',
     created() {
-      console.log('App created 5');
+      console.log('App created 6');
       this.loadDeprecatedShow();
     },
     methods: {
